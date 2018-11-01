a53ff550b4127e6521233136613a8f344bb80cb1
ma_parma <ma_parma@WIMBLE.ENCS.concordia.ca>
2018-10-31 20:31:53 -0400

    client code

--- Server/src/server/ClientFrame.java
+++ Server/src/server/ClientFrame.java
@@ -1,5 +1,106 @@
 package server;
 
-public class ClientFrame {
+import java.awt.Color;
+import java.awt.FlowLayout;//imports basic layout so we don't have to create one
 
+import javax.swing.JButton;
+import javax.swing.JFrame;// gives you basic features of windows (title bar, maximize, minimize...)
+import javax.swing.JLabel;//lets you output text and images
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;//ways for user to do something ex press enter
+import java.awt.event.ActionEvent;
+
+public class ClientFrame extends JFrame{
+	
+	private JLabel connectLabel2= new JLabel();
+	private JTextArea box= new JTextArea(5,20);
+	public JButton button= new JButton("Connect");
+	private ClientGenerator runClient = new ClientGenerator();
+	//Constructor
+	public ClientFrame() {
+		
+		super("Client TCP"); //gives title
+		//types of layout you can use: FlowLayout, GridLayout ...
+		setLayout(new FlowLayout()); //gives default layout
+		defaultConnection();
+		clickConnect();
+
+	}
+	
+	public void writeinBox2(String input) {
+		
+		box.append(input+"\n");
+	
+	}
+	
+
+	private void defaultConnection() {
+		
+		connectLabel2.setText("Connection Status: Not Connected");
+		connectLabel2.setForeground(Color.RED);
+		
+	}
+	
+	public void clickConnect() {
+		
+	button.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			checkConnection();
+	}
+
+	});
+	
+	}
+	
+	public void checkConnection() {
+		
+		if (button.getText().equals("Connect")) {
+			button.setText("Disconnect");
+			connectLabel2.setText("Connection Status: Connected" );
+			connectLabel2.setForeground(Color.BLUE);
+			
+			//runClient.startClient();//setConnection("Connected");
+		
+		}else {
+			button.setText("Connect");
+			connectLabel2.setText("Connection Status: Not Connected" );
+			connectLabel2.setForeground(Color.RED);
+		//runClient.stopClient();
+			
+		}
+	}
+	
+	public void setConnection(String connect) {
+		
+		if(connect.equals("Not Connected")) {
+			connectLabel2= new JLabel("Connection Status: "+ connect );
+			connectLabel2.setForeground(Color.RED);
+		
+			
+		}else {
+			connectLabel2= new JLabel("Connection Status: "+ connect );
+			connectLabel2.setForeground(Color.BLUE);
+		
+		}
+		
+	}
+	
+	public void displayFrame2() {
+		
+		add(connectLabel2);
+		add(box);
+		add(button);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(275,180);
+		setVisible(true);
+		
+	}
+	
+		
+	
+	
 }
--- Server/src/server/ClientGenerator.java
+++ Server/src/server/ClientGenerator.java
@@ -1,5 +1,88 @@
 package server;
 
-public class ClientGenerator {
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
 
+public class ClientGenerator{
+    
+    private static ClientFrame frame = new ClientFrame();
+    Socket clientSocket;
+	public void startClient() {
+		
+		try {
+			
+		//ip address: 127.0.0.1 ---> loopback ip address
+		//^ when connecting to yourself
+		//when connecting to someone else you have to know their ip address
+		clientSocket = new Socket("localhost",2245);
+		
+		new Thread() {
+			public void run() {
+				while(true) {
+		
+				//send message to the server
+				DataOutputStream outputToServer;
+				try {
+					outputToServer = new 
+							DataOutputStream(clientSocket.getOutputStream());
+					System.out.print("Client Name: ");
+					Scanner name = new Scanner(System.in);
+					String userName= name.nextLine();
+					
+					//use \n bc we use readline and it looks for new line character
+					String sentence=  "Connection Request from:"+ userName+ "\n";
+				
+					outputToServer.writeBytes(sentence);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				}
+			}
+		}.start();
+		//reads message from server
+		BufferedReader inputFromServer = new BufferedReader
+				(new InputStreamReader(clientSocket.getInputStream()));
+		
+		String recSentence = inputFromServer.readLine();
+		//frame.writeinBox2(recSentence);
+		System.out.println("from server: "+ recSentence);
+		
+    	}catch(Exception ex) {}
+		
+	}
+	
+	public void stopClient() {
+		
+      try {
+		
+        Socket clientSocket = new Socket("localhost",2245);
+        
+		// Send message to the server
+		DataOutputStream outputToServer = new 
+				DataOutputStream(clientSocket.getOutputStream());
+		
+		//use \n bc we use readline and it looks for new line character
+		
+		String data =  "Disconnection Request: Socket[addr=" + clientSocket.getLocalAddress() + ",port=" + clientSocket.getPort() + ",localport=" + clientSocket.getLocalPort() + "]\n";
+		
+        // Send data to server
+		outputToServer.writeBytes(data);
+		
+		// Client is no longer connected to Server
+		
+		
+      }catch(Exception ex) { }
+      
+	}
+  
+  
+	public static void main(String[] args) {
+      
+		ClientGenerator CG= new ClientGenerator();
+		CG.startClient();
+      
+	}
 }
